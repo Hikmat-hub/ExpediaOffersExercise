9c048e1eb5fd02ad459c54eab88d7b74c8394ba9
Hikmat  Shreim <hkshreim@redlink.com>
2019-10-21 18:20:33 +0300

    add another header to see if it will work with Heroku

--- src/main/java/com/expedia/exercise/dao/offer/OffersDaoImpl.java
+++ src/main/java/com/expedia/exercise/dao/offer/OffersDaoImpl.java
@@ -9,11 +9,7 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.CookieSpecs;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.params.ClientPNames;
-import org.apache.http.client.params.CookiePolicy;
 import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.impl.client.DecompressingHttpClient;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
@@ -21,16 +17,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 
 /**
@@ -85,28 +77,11 @@ public class OffersDaoImpl implements IOffersDao {
      * @throws IOException
      */
     private HttpResponse executeOfferRequest(URI offerServiceUri) throws IOException {
-
-//        HttpURLConnection conn = (HttpURLConnection) offerServiceUri.toURL().openConnection();
-//        conn.setRequestMethod("GET");
-////        conn.setRequestProperty("Accept", "application/json");
-//        conn.setRequestProperty(HttpHeaders.USER_AGENT, "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36");
-//        conn.setRequestProperty(HttpHeaders.ACCEPT, "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3");
-//        conn.setRequestProperty(HttpHeaders.ACCEPT_ENCODING, "gzip, deflate, br");
-//        conn.setRequestProperty(HttpHeaders.ACCEPT_LANGUAGE, "en-US,en;q=0.9");
-//        conn.setRequestProperty(HttpHeaders.CACHE_CONTROL, "no-cache");
-//        conn.setRequestProperty(HttpHeaders.CONNECTION, "keep-alive");
-//        conn.setRequestProperty(HttpHeaders.CONTENT_TYPE, "application/json");
-//        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-//        final String collect = br.lines().collect(Collectors.joining());
-//        System.out.println(collect);
-
         HttpClient httpClient = HttpClients.custom()
                 .setDefaultRequestConfig(RequestConfig.custom()
                         .setCookieSpec(CookieSpecs.STANDARD).build())
                 .setDefaultHeaders(Arrays.asList())
                 .build();
-
-//                new DecompressingHttpClient(new DefaultHttpClient());
         HttpGet httpGet = new HttpGet(offerServiceUri);
         addBrowserHeaders(httpGet);
         Arrays.stream(httpGet.getAllHeaders()).forEach(header -> LOGGER.warn(header.getName()+"--> " + header.getValue()));
@@ -125,6 +100,9 @@ public class OffersDaoImpl implements IOffersDao {
         httpGet.setHeader(HttpHeaders.CACHE_CONTROL, "no-cache");
         httpGet.setHeader(HttpHeaders.CONNECTION, "keep-alive");
         httpGet.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+        httpGet.setHeader(HttpHeaders.ACCEPT_CHARSET, "UTF-8");
+
+
     }
 
     /**
